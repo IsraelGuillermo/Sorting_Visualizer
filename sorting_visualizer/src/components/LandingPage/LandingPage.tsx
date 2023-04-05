0874451e4fb42d3b6f9bd6a1c501ea3a669c3fe1
Israel Guillermo <israelguillermo22@gmail.com>
2023-04-04 21:50:52 -0500

    sets up landing page

---
+++ sorting_visualizer/src/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,31 @@
+import { Box } from '@mui/material';
+import { useState } from 'react';
+import { ListInput } from '../ListInput';
+import { VisualList } from '../VisualList/VisualList';
+
+export function LandingPage() {
+  const [input, setInput] = useState('');
+  const [values, setValues] = useState<string[]>([]);
+  const [submitted, setSubmitted] = useState(false);
+
+  return (
+    <Box
+      height={'100vh'}
+      display='flex'
+      justifyContent='center'
+      alignItems='center'
+    >
+      {submitted ? (
+        <VisualList values={values} />
+      ) : (
+        <ListInput
+          setSubmitted={setSubmitted}
+          input={input}
+          setInput={setInput}
+          setValues={setValues}
+          values={values}
+        />
+      )}
+    </Box>
+  );
+}
